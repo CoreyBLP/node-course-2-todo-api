faadbf3f552d1efde2a3c7336bca7dcdb3abc1ac
Corey Callahan <corey@bottomlineperformance.com>
2018-06-22 15:39:27 -0400

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+var {ObjectID} = require ('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+
 
 var app = express();
 
@@ -26,11 +29,32 @@ app.get('/todos',(req,res)=>{
 	Todo.find().then((todos)=>{
 		res.send({
 			todos:todos
-		})
+		});
 	},(err)=>{
 		res.status(400).send(err);
 	})
-})
+});
+
+app.get('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+
+	if(ObjectID.isValid(id)){
+		Todo.findById(id).then((todo)=>{
+			if(!todo){
+				res.status(404).send();
+			}
+			res.send({
+				todo
+			});
+		},(err)=>{
+			res.status(400).send();
+		}).catch((e)=>{
+			res.status(400).send();
+		});
+	}else{
+		res.status(404).send();
+	}
+});
 
 app.listen(3000,()=>{
 	console.log("Connected on Port 3000");
